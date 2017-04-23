499151c56b25dfa6b38d5857a4ee1102088fc36e
Caprica0606 <caprica0606@gmail.com>
2017-04-23 11:47:39 -0400

    works, needs clean up

---
+++ battleCardGame/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<header>
+</header>
+  <body background = "battleCards/background.jpg">
+    <!-- Game Rules -->
+        <a href="battleCard.php"><img src = "battleCards/battleScroll.png"></a>
+</body>
+</html>
