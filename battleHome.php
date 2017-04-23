9ebd6145460ec1415dc92ac920dffb2d93b4e875
Caprica0606 <caprica0606@gmail.com>
2017-04-23 11:52:41 -0400

    Delete battleHome.php

--- battleHome.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<header>
-</header>
-  <body background = "battleCards/background.jpg">
-    <!-- Game Rules -->
-        <a href="battleCard.php"><img src = "battleCards/battleScroll.png"></a>
-</body>
-</html>
